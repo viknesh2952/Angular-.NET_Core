f0562ea8bc5d161dd63934dd5b844bbfcb163248
Viknesh Duraisamy <viknesh.duraisamy@bostonharborconsulting.com>
2024-06-03 13:27:02 +0530

    concepts added

--- src/app/concepts/template-properties/child_template/child-template.component.ts
+++ src/app/concepts/template-properties/child_template/child-template.component.ts
@@ -1,17 +1,29 @@
-import { Component, EventEmitter, Input, Output, QueryList, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, Output, SimpleChanges, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'child-template',//selector name changed here
   templateUrl: './child-template.component.html',
-  styleUrl: './child-template.component.css'
+  styleUrl: './child-template.component.css',
+  changeDetection:ChangeDetectionStrategy.OnPush // We can use this method or use detach, reattach with markForChanges() combination
 })
 export class ChildTemplateComponent {
 @Input('template') templateInput!:TemplateRef<any>;
+@Input('listInput') listInput!:number[];//this will not be tracked in ngOnChanges of child element by default as it is non-primitive data type
+@Input('InputEmitfromParent') InputEmitfromParent:any;
 @Output() afterClick:EventEmitter<any> = new EventEmitter();
+constructor(private changeDet: ChangeDetectorRef){
+
+}
 submit(){
   this.afterClick.emit('from child component');
 }
 valueFromChild(){
   return 'Accessing Child method from parent';
+}
+// ngOnChanges(changes:SimpleChanges){
+//   this.changeDet.detectChanges();
+// }
+refresh(){
+  this.changeDet.detectChanges();
 }
 }
--- src/app/concepts/template-properties/template-properties.component.ts
+++ src/app/concepts/template-properties/template-properties.component.ts
@@ -1,22 +1,38 @@
-import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { ChildTemplateComponent } from './child_template/child-template.component';
 
 @Component({
   selector: 'app-template-properties',
   templateUrl: './template-properties.component.html',
-  styleUrl: './template-properties.component.css'
+  styleUrls: ['./template-properties.component.css']
 })
-export class TemplatePropertiesComponent implements AfterViewInit {
-  @ViewChild('sampleTemplate') templateFromReference!:TemplateRef<any>;
-  @ViewChild(ChildTemplateComponent,{static:false}) childTemplate !:ChildTemplateComponent;
-  childMsg = ''
-  clickFun(event: any){
+export class TemplatePropertiesComponent implements OnInit, AfterViewInit {
+  @ViewChild('sampleTemplate') templateFromReference!: TemplateRef<any>;
+  @ViewChild(ChildTemplateComponent, { static: false }) childTemplate!: ChildTemplateComponent;
+  childMsg = '';
+  listValue: number[] = [];
+
+  constructor(private changeDet: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.listValue = [1, 2, 3];
+  }
+
+  clickFun(event: any) {
     this.childMsg = event;
   }
-  ngAfterViewInit(){
+
+  ngAfterViewInit() {
     console.log('Child has been initialized!');
   }
-  childMethod(){
+
+  childMethod() {
     alert(this.childTemplate.valueFromChild());
   }
+
+  addElement() {
+    let val = this.listValue.length-1;
+    this.listValue.push(val); //this won't detect changes in onpush strategy
+    // this.listValue = [...this.listValue,val]; //this will detect changes in onpush strategy as it is spread 
+  }  
 }
